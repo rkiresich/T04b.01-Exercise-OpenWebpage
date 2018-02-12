1d3473e7adf0691409a266d87031b95458ee0f23
Ricky K <rkiresich@gmail.com>
2018-02-11 21:08:57 -0700

    Completed, awesome to start seeing things comes together in a more complex subject

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -37,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
     public void onClickOpenWebpageButton(View v) {
         // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
 
+        String url = "http://www.udacity.com";
         // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+
+        openWebPage(url);
     }
 
     /**
@@ -83,10 +87,18 @@ public class MainActivity extends AppCompatActivity {
     private void openWebPage(String url) {
 
         // TODO (2) Use Uri.parse to parse the String into a Uri
+        //COMPLETE
+        Uri uri = Uri.parse(url);
 
         // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
+        //COMPLETE
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 
         // TODO (4) Verify that this Intent can be launched and then call startActivity
+        //COMPLETE
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
 
     }
 }
